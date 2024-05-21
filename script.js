4897e192467c01d3134955b4dd885cf988472769
Julian Andersen <julian.cesar.andersen@gmail.com>
2024-05-21 16:02:21 +0200

    Create function to get computer choice

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 // print hello world to the console
 console.log("hello world");
+
+// get the computer choice
+
+function getComputerChoice() {
+    let computerChoice;
+    const computerChoiceNumber = Math.floor(Math.random() * 3);
+    console.log(computerChoiceNumber);
+
+    switch (computerChoiceNumber) {
+        case 0:
+            computerChoice = "Rock";
+            break;
+        case 1:
+            computerChoice = "Paper";
+            break;
+        case 2:
+            computerChoice = "Scissor";
+            break;
+    }
+}
